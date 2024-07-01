6c49f903d80eab3d0f0138358cb211c974f39ea0
rukenya321 <rukenyamunenejoel@gmail.com>
2024-07-01 11:34:51 +0300

    new changes

--- src/student/summaryPRO/summaryPRO.js
+++ src/student/summaryPRO/summaryPRO.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Apr 20, 2024 12:00:00").getTime();
+var countDownDate = new Date("July 26, 2024 12:00:00").getTime();
 
 // Update the countdown every 1 second
 var x = setInterval(function() {
